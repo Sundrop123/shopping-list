b687ebe1fad56db421e3a81fcd73f4906a3e0f40
Sundrop123 <47043481+Sundrop123@users.noreply.github.com>
2019-03-02 18:05:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,15 @@
-$(document).ready(initializeForm);
+const store = {
+  items: [
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ],
+  hideCheckedItems: false,
+  searchTerm: ''
+};
 
-function initializeForm() {
-
-    
-    var itemCount = 0;
-    var itemInput = $("input#item");
-    itemInput.val("");
-
-    $("a#add-item").click(shoppingList);
-    $("#item").keydown(function (enter) {
-        if (enter.keyCode == 13) {
-            shoppingList();
-        }
-    });
-
-    function shoppingList() {
-        if (itemInput.val() === '') {
-            return;
-        }
-        // get the items list and item to add
-        var items = $("ul#items");
-        var purchased = $("ul#purchased");
-        var itemToBuy = itemInput.val();
-        itemInput.val("");
-
-        var listItem = $("<li><input type='checkbox' name=" + itemToBuy + " value=" + itemToBuy + "> " + itemToBuy + " <a class='delete' href='#'><strong>-</strong> Delete</a></li>");
-        listItem.attr("id", "item[" + itemCount+++"]");
-        addItem(listItem);
-
-        
-        listItem.find("a").click(function () {
-            $(this).parent().hide('slow', function () {
-                $(this).remove();
-            });
-        });
-
-      
-        function addItem(listItem) {
-            listItem.hide();
-            items.append(listItem);
-            listItem.show('slow');
-        }
-    
-        function purchasedItem(listItem) {
-            listItem.hide();
-            purchased.append(listItem);
-            listItem.show('slow');
-        }
-        
-        listItem.find("input:checkbox").click(function () {
-            this.checked ? purchasedItem(listItem) : addItem(listItem);
-        });
-
-        itemInput.focus();
-    }
-}
+$(document).ready(function() {
+  shoppingList.bindEventListeners();
+  shoppingList.render();
+});
