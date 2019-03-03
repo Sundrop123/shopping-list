a79579a38a5269239fe568c261d18c27f6dc0870
Sundrop123 <47043481+Sundrop123@users.noreply.github.com>
2019-03-03 09:16:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,116 +1,31 @@
-var state = {
-  items: []
-};
-
-
-var listItemTemplate = (
-  '<li>' +
-    '<span class="shopping-item js-shopping-item"></span>' +
-    '<div class="shopping-item-controls">' +
-      '<button class="js-shopping-item-toggle">' +
-        '<span class="button-label">check</span>' +
-      '</button>' +
-      '<button class="js-shopping-item-delete">' +
-        '<span class="button-label">delete</span>' +
-      '</button>' +
-    '</div>' +
-  '</li>'
-);
-
-function addItem(state, item) {
-  state.items.push({
-    displayName: item,
-    checkedOff: false
-  });
-}
-
-function getItem(state, itemIndex) {
-  return state.items[itemIndex];
-}
-
-function deleteItem(state, itemIndex) {
-  state.items.splice(itemIndex, 1);
-}
-
-function updateItem(state, itemIndex, newItemState) {
-  state.items[itemIndex] = newItemState;
-}
-
-
-
-function renderItem(item, itemId, itemTemplate, itemDataAttr) {
-  var element = $(itemTemplate);
-  element.find('.js-shopping-item').text(item.displayName);
-  if (item.checkedOff) {
-    element.find('.js-shopping-item').addClass('shopping-item__checked');
-  }
-  element.find('.js-shopping-item-toggle')
-  element.attr(itemDataAttr, itemId);
-  return element;
-}
-
-function renderList(state, listElement, itemDataAttr) {
-  var itemsHTML = state.items.map(
-    function(item, index) {
-      return renderItem(item, index, listItemTemplate, itemDataAttr);
-  });
-  listElement.html(itemsHTML);
-}
-
-
-
-function handleItemAdds(
-  formElement, newItemIdentifier, itemDataAttr, listElement, state) {
-
-  formElement.submit(function(event) {
-    event.preventDefault();
-    var newItem = formElement.find(newItemIdentifier).val();
-    addItem(state, newItem);
-    renderList(state, listElement, itemDataAttr);
-    this.reset();
-  });
-}
-
-function handleItemDeletes(
-  formElement, removeIdentifier, itemDataAttr, listElement, state) {
-
-  listElement.on('click', removeIdentifier, function(event) {
-    var itemIndex = parseInt($(this).closest('li').attr(itemDataAttr));
-    deleteItem(state, itemIndex);
-    renderList(state, listElement, itemDataAttr);
-  })
-}
-
-
-function handleItemToggles(
-  listElement, toggleIdentifier, itemDataAttr, state) {
-
-  listElement.on('click', toggleIdentifier, function(event) {
-    var itemId = $(event.currentTarget.closest('li')).attr(itemDataAttr);
-    var oldItem = getItem(state, itemId);
-
-    updateItem(state, itemId, {
-      displayName: oldItem.displayName,
-      checkedOff: !oldItem.checkedOff
-    });
-    renderList(state, listElement, itemDataAttr)
-  });
-}
-
-
-$(function() {
-  var formElement = $('#js-shopping-list-form');
-  var listElement = $('.js-shopping-list');
-  var newItemIdentifier = '#js-new-item';
-  var removeIdentifier = '.js-shopping-item-delete';
-  var itemDataAttr = 'data-list-item-id';
-  var toggleIdentifier = '.js-shopping-item-toggle'
+$(function(){
+    $('#js-shopping-list-form').submit(function(event) {
+      event.preventDefault();
+      const shoppingListItem = $('#shopping-list-entry').val();
+    $('#shopping-list-entry').val('');
   
-  handleItemAdds(
-    formElement, newItemIdentifier, itemDataAttr, listElement, state);
-  handleItemDeletes(
-    formElement, removeIdentifier, itemDataAttr, listElement, state);
-  handleItemToggles(listElement, toggleIdentifier, itemDataAttr, state);
-});
+      $('.shopping-list').append(
+        `<li>
+          <span class="shopping-item">${shoppingListItem}</span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>`);
+    
+      });
+  
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+   
+      $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    
+     });
+     $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+      $(this).closest('li').remove();
+  });
 
-
+  });
